e1f3cc0c9604e50878048cce7a8903dd0f164a1e
tranquochuy2210 <tranquochuy221096@gmail.com>
2024-06-22 11:46:31 +0700

    init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=project3DB
+export POSTGRES_PASSWORD=db.simba03
+export POSTGRES_HOST=database-1.czwofcvltwkw.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::project3-435735611281-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
